073da100aba20c179676b3b8bdcb96ec85dd6ad6
YupengLin <linyupeng2400@hotmail.com>
2015-11-12 18:38:25 -0500

    element shopping cart

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,7 +1,32 @@
 class PayController < ApplicationController
+  @@shopItem = Array.new(0)
   def shoppingCart
+  	num = params[:quantity].to_i
+  	for i in 0 .. num - 1
+  		@@shopItem.push(params[:type])
+  	end
   end
 
   def checkout
   end
+
+  def totalItemNumber
+  	@@shopItem.length
+  end 
+
+  def self.shopItem
+  	@@shopItem
+  end
+
+  def totalPrice
+  	coffeeMenu = ShoppingController.menu
+  	priceSum = 0
+  	for i in 0 .. @@shopItem.length - 1
+  		priceSum = priceSum + coffeeMenu[@@shopItem.at(i)]
+  	end
+    priceSum
+  end
+
+  helper_method :totalItemNumber
+  helper_method :totalPrice
 end
--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,9 +1,17 @@
 class ShoppingController < ApplicationController
-	@@menu = {"latte" => 5.0, "mocha" => 4.0, "iceCoffee" => 3.0, "hotChocolate" => 2.0}
+	@@menu = {"latte" => 5.0, "mocha" => 4.0, "iceCoffee" => 3.0, "hotChocolate" => 2.0, "HazelNut" => 15.0,"TastyCake" => 19.0, "SweetChok" => 21.0, "DelicioChocolate" => 23.0}
+
   def allItem
   end
 
   def singleProduct
+  	@productName = "latte"
+  	@unitPrice = 5.0
+  	if request.query_string.present?
+
+  		@productName = params[:type]
+  		@unitPrice = @@menu[params[:type]]
+  	end
   end
 
   def self.menu
@@ -16,7 +24,21 @@ class ShoppingController < ApplicationController
   	@@menu[name]
   end
 
- 
+  def totalItemNumber
+  	 PayController.shopItem.length
+  end
+
+  def getUnitPrice
+  	@unitPrice
+  end
+
+  def getProductName
+  	 @productName
+  end
+
+  helper_method :totalItemNumber
   helper_method :getProduct
+  helper_method :getUnitPrice
+  helper_method :getProductName
 
 end
