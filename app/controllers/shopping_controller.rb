64baa1722e110facdd5f955a9fe5291ba8b759c9
YupengLin <linyupeng2400@hotmail.com>
2015-11-12 16:00:11 -0500

    add the hashmap products

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,7 +1,22 @@
 class ShoppingController < ApplicationController
+	@@menu = {"latte" => 5.0, "mocha" => 4.0, "iceCoffee" => 3.0, "hotChocolate" => 2.0}
   def allItem
   end
 
   def singleProduct
   end
+
+  def self.menu
+  	@@menu
+  end
+
+  
+
+  def getProduct(name)
+  	@@menu[name]
+  end
+
+ 
+  helper_method :getProduct
+
 end
