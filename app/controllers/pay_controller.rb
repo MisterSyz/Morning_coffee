775aa9ed7854a8cd21e08c7dfff467ecd41c6b51
YupengLin <linyupeng2400@hotmail.com>
2015-11-12 21:59:08 -0500

    fix shopping cart

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,46 +1,74 @@
 class PayController < ApplicationController
   @@shopItem = Array.new(0)
+  @@itemSummary =  Hash.new(0)
+
  
   def shoppingCart
-  	num = params[:quantity].to_i
-  	for i in 0 .. num - 1
-  		@@shopItem.push(params[:type])
-  	end
+    num = params[:quantity].to_i
+    for i in 0 .. num - 1
+      @@shopItem.push(params[:type])
+      if(@@itemSummary.has_key?(params[:type]))
+        freq = @@itemSummary[params[:type]] + 1
+        @@itemSummary[params[:type]] = freq
+      else
+        @@itemSummary.store(params[:type], 1)
+      end
+
+    end
+   
   end
 
   def checkout
   end
 
   def totalItemNumber
-  	@@shopItem.length
+    @@shopItem.length
   end 
 
   def self.shopItem
-  	@@shopItem
+    @@shopItem
   end
 
   def totalPrice
-  	coffeeMenu = ShoppingController.menu
-  	priceSum = 0
-  	for i in 0 .. @@shopItem.length - 1
-  		priceSum = priceSum + coffeeMenu[@@shopItem.at(i)]
-  	end
+    coffeeMenu = ShoppingController.menu
+    priceSum = 0
+    for i in 0 .. @@shopItem.length - 1
+      priceSum = priceSum + coffeeMenu[@@shopItem.at(i)]
+    end
     priceSum
   end
 
  
-  def getSingleProductName(index)
-  	@@shopItem.at(index)
+ 
+## get the unit price by index, the index is the itemSummary index
+  def getUnitPrice(index)
+    coffeeMenu = ShoppingController.menu
+    keys = @@itemSummary.keys
+    coffeeMenu[keys[index]]
   end
 
-  def getUnitPrice(index)
-  	coffeeMenu = ShoppingController.menu
-  	coffeeMenu[@@shopItem.at(index)]
-  
+ 
+
+  def getUniqueItemHashLength
+    @@itemSummary.length
   end
+
+  def getUniqueItemName(index)
+    keys = @@itemSummary.keys
+    keys[index]
+  end
+
+  def getUniqueItemNumber(index)
+    keys = @@itemSummary.keys
+    @@itemSummary[keys[index]]
+  end
+
   
-  helper_method :getSingleProductName
+
   helper_method :getUnitPrice
   helper_method :totalItemNumber
   helper_method :totalPrice
+  helper_method :getUniqueItemNumber
+  helper_method :getUniqueItemName
+  helper_method :getUniqueItemHashLength
 end
