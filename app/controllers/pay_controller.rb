be837f611ee97ae42a542cab94ba3130b3e38033
Yuzhuo Sun <Sun@dhcp-hol-3945.redrover.cornell.edu>
2015-11-12 20:47:06 -0500

    test

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,5 +1,6 @@
 class PayController < ApplicationController
   @@shopItem = Array.new(0)
+ 
   def shoppingCart
   	num = params[:quantity].to_i
   	for i in 0 .. num - 1
@@ -27,6 +28,19 @@ class PayController < ApplicationController
     priceSum
   end
 
+ 
+  def getSingleProductName(index)
+  	@@shopItem.at(index)
+  end
+
+  def getUnitPrice(index)
+  	coffeeMenu = ShoppingController.menu
+  	coffeeMenu[@@shopItem.at(index)]
+  
+  end
+  
+  helper_method :getSingleProductName
+  helper_method :getUnitPrice
   helper_method :totalItemNumber
   helper_method :totalPrice
 end
