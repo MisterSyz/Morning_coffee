38cb5ae286fc613c33be2a1335362b4962314d43
YupengLin <linyupeng2400@hotmail.com>
2015-11-12 21:49:01 -0500

    fix shopping cart

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,10 +1,21 @@
 class PayController < ApplicationController
   @@shopItem = Array.new(0)
+  @@itemSummary =  Hash.new(0)
+
+ 
   def shoppingCart
   	num = params[:quantity].to_i
   	for i in 0 .. num - 1
   		@@shopItem.push(params[:type])
+  		if(@@itemSummary.has_key?(params[:type]))
+  			freq = @@itemSummary[params[:type]] + 1
+  			@@itemSummary[params[:type]] = freq
+  		else
+  			@@itemSummary.store(params[:type], 1)
+  		end
+
   	end
+   
   end
 
   def checkout
@@ -27,6 +38,37 @@ class PayController < ApplicationController
     priceSum
   end
 
+ 
+ 
+## get the unit price by index, the index is the itemSummary index
+  def getUnitPrice(index)
+  	coffeeMenu = ShoppingController.menu
+  	keys = @@itemSummary.keys
+  	coffeeMenu[keys[index]]
+  end
+
+ 
+
+  def getUniqueItemHashLength
+  	@@itemSummary.length
+  end
+
+  def getUniqueItemName(index)
+  	keys = @@itemSummary.keys
+  	keys[index]
+  end
+
+  def getUniqueItemNumber(index)
+  	keys = @@itemSummary.keys
+  	@@itemSummary[keys[index]]
+  end
+
+  
+
+  helper_method :getUnitPrice
   helper_method :totalItemNumber
   helper_method :totalPrice
+  helper_method :getUniqueItemNumber
+  helper_method :getUniqueItemName
+  helper_method :getUniqueItemHashLength
 end
